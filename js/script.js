bf3e5f09eb461d44269ca0fac8c55fe465eb97fe
Simone-Ortu <simo.28.09o@gmail.com>
2021-02-10 17:30:43 +0100

    Non funziona niente

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        apiKey:'96b596d61163d8fd4f87683973cabeb5',
+        query:'',
+        films: []
+    },
+    methods:{
+        search(){
+            axios
+            .get('https://api.themoviedb.org/3/search/movie',{
+                params:{
+                    api_key:this.apiKey,
+                    query: this.query,
+                    language: 'it-IT'
+                }
+            })
+            .then((result)=> {
+                this.films = result.data.result;
+                
+
+            })
+            .catch((error)=> alert('errore'));
+        }
+
+    }
+})
