c2f56cef2c2055fc62efedf60863242507e1a276
Simone-Ortu <simo.28.09o@gmail.com>
2021-02-12 03:31:11 +0100

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,54 @@
 var app = new Vue({
-    el:'#app',
-    data:{
-        apiKey:'96b596d61163d8fd4f87683973cabeb5',
-        query:'',
-        films: []
+    el: '#app',
+    data: {
+        apiKey: '96b596d61163d8fd4f87683973cabeb5',
+        query: '',
+        films: [],
+        origLang: [],
+        flags:[] 
     },
-    methods:{
-        search(){
+    methods: {
+        search() {
             axios
-            .get('https://api.themoviedb.org/3/search/movie',{
-                params:{
-                    api_key:this.apiKey,
-                    query: this.query,
-                    language: 'it-IT'
-                }
-            })
-            .then((result)=> {
-                this.films = result.data.results;
-                console.log(result.data.results);
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: this.apiKey,
+                        query: this.query,
+                        language: 'it-IT'
+                    }
+                })
+                .then((result) => {
+                    this.films = result.data.results;
+                    console.log(result.data.results);
+
+                    axios
+                .get("https://api.themoviedb.org/3/search/tv", {
+                    params: {
+                        api_key: this.apiKey,
+                        query: this.query,
+                        language: 'it-IT'
+                    }
+                })
+                .then((resultSeries) => {
+                    this.films = [...this.films, ...resultSeries.data.results];
+                    console.log(result.data.results);
+
+                })
                 
 
-            })
-            .catch((error)=> alert('errore'));
-        }
+
+                })
+                .catch((error) => alert(error));
+
+
+            
+            },
+            getStars(vote){
+                return parseInt(vote / 2)
+            },
+            diffStars(vote){
+                return 5 - parseInt(vote/2)
+            },
 
     }
 })
